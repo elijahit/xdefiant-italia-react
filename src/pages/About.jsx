daa2b62856019f62ec221ac47c54d3f51d19693d
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-15 13:14:51 +0200

    END: Pagina about in beta

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,44 @@
 import "./About.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
 
 function About() {
   document.title = "Pizzeria - Chi Siamo"
   return (
     <>
-      <Header isPage="about"/>
-      <p>test menu</p>
+      <Header isPage="about" />
+      <HeroSection/>
+      <div className="container mt-2 mb-5">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-center">Chi siamo</h1>
+            <p className="text-center fs-5">Benvenuti nella community italiana di XDefiant! Fondata nel 2021, la nostra missione è quella di tenere i giocatori sempre aggiornati su tutte le novità, gli eventi speciali e le competizioni legate a XDefiant. Siamo appassionati del gioco e crediamo nel potere della community di migliorare l'esperienza di gioco per tutti i membri.
+
+              Utilizziamo una varietà di canali social per fornire informazioni tempestive, tra cui aggiornamenti su patch, nuovi contenuti, e iniziative della community. La nostra piattaforma è progettata per essere un punto di riferimento dove i giocatori possono trovare notizie fresche, guide dettagliate e strategie per migliorare il proprio gameplay.
+
+              Oltre a mantenere i nostri membri informati, ci impegniamo a creare un ambiente accogliente e inclusivo. Incoraggiamo la condivisione di esperienze e la discussione di strategie, promuovendo una cultura di rispetto e collaborazione. Organizziamo regolarmente tornei e eventi speciali che offrono ai membri l'opportunità di mettere alla prova le loro abilità, divertirsi e connettersi con altri appassionati di XDefiant.
+
+              Invitiamo tutti i fan a unirsi a noi per rimanere sempre aggiornati e far parte di una community vibrante e attiva. Partecipare alla nostra community significa non solo avere accesso alle ultime notizie e aggiornamenti, ma anche entrare a far parte di una famiglia di giocatori che condivide la tua passione per XDefiant. Unitevi a noi e diventate parte della nostra avventura in XDefiant, dove il gioco e la community si incontrano per creare un'esperienza unica e indimenticabile!</p>
+            <h2 className="text-center">Perché Sceglierci?</h2>
+            <p className="fs-5">Scegliere la community italiana di XDefiant significa entrare a far parte di un gruppo di appassionati che condividono la tua stessa passione per il gioco. Ecco perché dovresti unirti a noi:</p>
+            <ul>
+              <li><strong>Aggiornamenti Costanti e Puntuali:</strong> Rimani sempre informato su tutte le ultime novità, gli aggiornamenti e gli eventi speciali di XDefiant. Utilizziamo una varietà di canali social per assicurarti di non perdere mai un'informazione importante.</li>
+              <li><strong>Ambiente Accogliente e Inclusivo:</strong> La nostra community è progettata per essere un luogo dove tutti i giocatori possono sentirsi a casa. Incoraggiamo la condivisione di esperienze, la discussione di strategie e promuoviamo una cultura di rispetto e collaborazione.</li>
+              <li><strong>Eventi e Competizioni:</strong> Organizziamo regolarmente tornei ed eventi speciali che offrono ai nostri membri la possibilità di mettere alla prova le loro abilità, divertirsi e connettersi con altri appassionati di XDefiant.</li>
+              <li><strong>Risorse Utili e Guide:</strong> Forniamo guide dettagliate e strategie per aiutarti a migliorare il tuo gameplay. Che tu sia un principiante o un giocatore esperto, troverai sempre qualcosa di utile per affinare le tue competenze.</li>
+              <li><strong>Supporto e Interazione:</strong> Siamo sempre disponibili per aiutarti con qualsiasi domanda o dubbio riguardante il gioco. La nostra community è attiva e pronta a supportarti, rendendo la tua esperienza di gioco ancora più piacevole.</li>
+              <li><strong>Unisciti a una Community Vibrante:</strong> Far parte della nostra community significa entrare a far parte di una famiglia di giocatori che condividono la tua passione per XDefiant. La nostra comunità è vivace, attiva e sempre pronta a nuove sfide e avventure.</li>
+            </ul>
+            <h2 className="text-center">Questo sito web è in beta</h2>
+            <p className="fs-5">Il sito di XDefiant Italia è in versione beta, il che significa che stiamo ancora lavorando per perfezionare l'esperienza utente e migliorare tutte le funzionalità offerte. Durante questa fase, potreste incontrare alcune aree in fase di sviluppo o contenuti in aggiornamento.
+
+              Ci teniamo a ringraziarvi per la vostra pazienza e il vostro supporto mentre continuiamo a migliorare. I vostri feedback sono estremamente preziosi per noi e ci aiutano a creare un sito migliore. Non esitate a contattarci per condividere le vostre opinioni o segnalare eventuali problemi che potreste incontrare.
+
+              Grazie per essere parte della nostra community e per il vostro contributo nel rendere questo sito web una risorsa preziosa per tutti i fan di XDefiant!</p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );

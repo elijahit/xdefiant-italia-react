15a386696120c6709e129356af2e6ce730da1094
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-17 19:18:10 +0200

    RESOLVE: Risolto il problema del routes, ora funziona correttamente

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 import About from './pages/About.jsx';
 import Menu from './pages/Menu.jsx';
 import ErrorPage from './pages/ErrorPage.jsx';
-import Posts from './pages/Posts.jsx';
+import PenalitaJump from './pages/posts/PenalitaJump.jsx';
+
+const postElement = [
+  {
+    path: "posts/penalita-spamming",
+    element: <PenalitaJump />
+  }
+]
 
 const router = createBrowserRouter([
   {
@@ -19,9 +26,14 @@ const router = createBrowserRouter([
     element: <About />
   },
   {
-    path: "/posts/*",
-    element: <Posts/>,
-  }
+    path: "/",
+    element: <Outlet/>,
+    errorElement: <ErrorPage />,
+    children: [
+      ...postElement
+    ],
+  },
+  
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(

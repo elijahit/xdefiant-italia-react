60856c0b5b9eede208801810e083dda6c0023632
Elijah <64620855+elijahit@users.noreply.github.com>
2024-06-17 15:10:23 +0200

    ADD: posts penalità

--- src/pages/posts/PenalitaJump.jsx
+++ src/pages/posts/PenalitaJump.jsx
@@ -22,16 +22,16 @@ function PenalitaJump() {
             </p>
             <article>
               <h2>Come Funzionano queste Penalità?</h2>
-              <p>Le penalità si attivano dopo tre salti o accovacciate consecutive. Da quel momento, la velocità di queste azioni rallenta drasticamente e la precisione di mira subisce una penalizzazione significativa, causando una dispersione maggiore dei colpi. La mira inizia a seguire una traiettoria simile a un simbolo dell'infinito (∞). Questa traiettoria non solo aumenta progressivamente di velocità, ma inverte anche casualmente la direzione. Questo rende ancora più difficile mantenere la mira su un bersaglio, costringendo i giocatori a usare questi movimenti con maggiore attenzione e a optare per tattiche più ponderate, mantenendo il gioco più strategico e meno caotico.</p>
+              <p className="fs-5">Le penalità si attivano dopo tre salti o accovacciate consecutive. Da quel momento, la velocità di queste azioni rallenta drasticamente e la precisione di mira subisce una penalizzazione significativa, causando una dispersione maggiore dei colpi. La mira inizia a seguire una traiettoria simile a un simbolo dell'infinito (∞). Questa traiettoria non solo aumenta progressivamente di velocità, ma inverte anche casualmente la direzione. Questo rende ancora più difficile mantenere la mira su un bersaglio, costringendo i giocatori a usare questi movimenti con maggiore attenzione e a optare per tattiche più ponderate, mantenendo il gioco più strategico e meno caotico.</p>
 
               <h2>Effetti sul Gameplay</h2>
-              <p>Le penalità hanno un impatto rilevante sul gameplay limitando l'efficacia delle tattiche di movimento rapido, queste misure costringono i giocatori a pensare in modo più strategico e a migliorare la loro precisione senza fare affidamento su salti e accovacciamenti ripetuti.</p>
+              <p className="fs-5">Le penalità hanno un impatto rilevante sul gameplay limitando l'efficacia delle tattiche di movimento rapido, queste misure costringono i giocatori a pensare in modo più strategico e a migliorare la loro precisione senza fare affidamento su salti e accovacciamenti ripetuti.</p>
               
               <h2>Reazioni della Community</h2>
-              <p>La community di XDefiant ha accolto positivamente queste modifiche, apprezzando l'equilibrio che portano al gioco. Tuttavia, alcuni giocatori hanno espresso preoccupazioni riguardo alla possibile riduzione della libertà di movimento e dell'agilità che sono caratteristiche distintive del gioco.</p>
+              <p className="fs-5">La community di XDefiant ha accolto positivamente queste modifiche, apprezzando l'equilibrio che portano al gioco. Tuttavia, alcuni giocatori hanno espresso preoccupazioni riguardo alla possibile riduzione della libertà di movimento e dell'agilità che sono caratteristiche distintive del gioco.</p>
 
               <h2>Conclusione</h2>
-              <p>Le penalità per il jump e crouch spamming in XDefiant rappresentano un passo importante verso un'esperienza di gioco più bilanciata. Questo aggiornamento riflette l'impegno di Ubisoft nel migliorare il gioco e rispondere ai feedback dei giocatori.</p>
+              <p className="fs-5">Le penalità per il jump e crouch spamming in XDefiant rappresentano un passo importante verso un'esperienza di gioco più bilanciata. Questo aggiornamento riflette l'impegno di Ubisoft nel migliorare il gioco e rispondere ai feedback dei giocatori.</p>
               <img src={image1} className="img-fluid mx-auto d-block mb-5" width={700} height={400} alt="Immagine che rappresenta il movimento dell'arma" />
             </article>
           </div>
